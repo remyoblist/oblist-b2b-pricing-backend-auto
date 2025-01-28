553d571c49f89b0a8fad0e11ba63be175c1a737d
mericanAncer <vicenteputheany701@gmail.com>
2025-01-28 08:52:28 +0100

    fix: v1.0 monodb connect

--- db/config.js
+++ db/config.js
@@ -1,10 +1,23 @@
 //import mongoose in the application
 const mongoose = require('mongoose');
 
+const mongoURI = process.env.MONGODB_URI;
+
+if (!mongoURI) {
+  console.error("Error: MONGODB_URI is not defined in environment variables.");
+  process.exit(1); // Exit the application if MONGODB_URI is not set
+}
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+})
+.then(() => {
+  console.log("Connected to MongoDB successfully.");
+})
+.catch((error) => {
+  console.error("Error connecting to MongoDB:", error);
+  process.exit(1); // Exit the application if the connection fails
+});;
 
 module.exports = mongoose;
